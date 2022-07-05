04a3ed2cf115deea41fe72e0a31038190724c9aa
William Emanuelsson <wem@kth.se>
2022-07-05 23:45:37 +0200

    Merge

--- README.m
+++ README.m
@@ -1,1 +1,3 @@
-# News headline generator
+# Game of Thrones  
+
+Are you sad that your favorite TV-show has ended? Fear not, because this ML-model will let you generate unlimited amounts of GOT-content, albeit in the form of text.
